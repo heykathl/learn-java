a0709b79bf2ff0004122a8915195fc89dff87a98
Kathleen Ly <heykathl@users.noreply.github.com>
2022-07-08 22:12:11 +0100

    Created calculator class

---
+++ Calculator/Calculator.java
@@ -0,0 +1,8 @@
+package Calculator;
+
+public class Calculator {
+  public static void main(String[] args){
+    Integer result = 2 + 2;
+    System.out.printf("The result is %d \n", result);
+  }
+}
