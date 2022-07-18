c43e658636842df1a058b4f5f0d5b9fa285dc52f
Kathleen Ly <heykathl@users.noreply.github.com>
2022-07-18 10:07:56 +0100

    Calculator class to print out numerous decimal point integers

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -2,7 +2,7 @@ package Calculator;
 
 public class Calculator {
   public static void main(String[] args){
-    Integer result = 2 + 2;
-    System.out.printf("The result is %d \n", result);
+    Double result = 2.5 + 2.5;
+    System.out.printf("The result is %.6f \n", result);
   }
 }
