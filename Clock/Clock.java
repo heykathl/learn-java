9311175bad785189214689729598b720dcef471e
Kathleen Ly <heykathl@users.noreply.github.com>
2022-07-18 09:59:56 +0100

    Created clock class

---
+++ Clock/Clock.java
@@ -0,0 +1,13 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class Clock {
+
+    public static void main(String[] args) {
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm");
+        LocalDateTime now = LocalDateTime.now();
+        String red = "\033[31m";
+        String resetColour = "\033[0m";
+        System.out.println("The current " + red + "time" + resetColour + " is " + time.format(now));
+    }
+}
