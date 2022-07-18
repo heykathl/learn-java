4d14e63159c45c644681661ec6baaa0b00abc3f1
Kathleen Ly <heykathl@users.noreply.github.com>
2022-07-18 10:14:35 +0100

    Created priceDisplay class

---
+++ PriceDisplay/PriceDisplay.java
@@ -0,0 +1,9 @@
+
+public class PriceDisplay {
+
+    public static void main(String[] args) {
+        String item = "Coffee";
+        Double price = 5.99;
+        System.out.printf("The price is the item %s is £%.2f \n", item, price);
+    }
+}
